6961415b4a5083a99c07411b5f82962642e62e8c
whatislove <vla05757@gmail.com>
2025-04-18 22:52:11 +0300

    added service layer realization

--- new_version/internal/service/transaction.service.go
+++ new_version/internal/service/transaction.service.go
@@ -1,11 +1,36 @@
 package service
 
-import "merch_service/new_version/internal/storage"
+import (
+	"context"
+	"merch_service/new_version/internal/storage"
+)
 
 type TransactionServiceInterface interface {
-	Send(sender, recv string, amount int) error
+	// Send - отправляет amount монет от sender к recv
+	Send(ctx context.Context, sender, recv string, amount int) error
 }
 
+var _ TransactionServiceInterface = (*TransactionService)(nil)
+
+// TransactionService - реализует интерфейс TransactionServiceInterface
 type TransactionService struct {
 	TransactionStorage storage.Storage
+}
+
+func (t *TransactionService) Send(ctx context.Context, sender, recv string, amount int) error {
+	sendUser, err := t.TransactionStorage.FindUserByLogin(ctx, sender)
+	if err != nil {
+		return err
+	}
+
+	recvUser, err := t.TransactionStorage.FindUserByLogin(ctx, recv)
+	if err != nil {
+		return err
+	}
+
+	err = t.TransactionStorage.MakeTransaction(ctx, sendUser, recvUser, amount)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- new_version/internal/service/user.service.go
+++ new_version/internal/service/user.service.go
@@ -1,20 +1,83 @@
 package service
 
 import (
+	"context"
+	"fmt"
 	"merch_service/new_version/internal/models"
 	"merch_service/new_version/internal/storage"
 )
 
 type UserServiceInterface interface {
-	Login(logReq *models.LoginRequest) error
-	
-	Register(logReq *models.LoginRequest) error
+	// Login - проверяет есть ли такой пользователь
+	// и, если был возвращет nil вместо ошибки
+	Login(ctx context.Context, logReq *models.LoginRequest) error
 
-	CoinsHistory(login string) ([]models.CoinsEntry, error)
+	// Register - проверяет не было ли такого пользователя уже
+	// и, если не было, добавляет его и возвращает nil
+	Register(ctx context.Context, regReq *models.LoginRequest) error
 
-	PurchaseHistory(login string) ([]models.PurchaseEntry, error)
+	// CoinsHistory - возвращает слайс с историей изменения баланса
+	CoinsHistory(ctx context.Context, login string) ([]models.CoinsEntry, error)
+
+	// PurchaseHistory - возвращает слайс с историей покупок мерча
+	PurchaseHistory(ctx context.Context, login string) ([]models.PurchaseEntry, error)
 }
 
+var _ UserServiceInterface = (*UserService)(nil)
+
+// UserService - реализует интерфейс UserServiceInterface
 type UserService struct {
 	UserStorage storage.Storage
+}
+
+func (u *UserService) Login(ctx context.Context, logReq *models.LoginRequest) error {
+	user, err := u.UserStorage.FindUserByLogin(ctx, logReq.Login)
+	if err != nil {
+		return err
+	}
+
+	if user.Password != logReq.Password {
+		return fmt.Errorf("неверный пароль")
+	}
+
+	return nil
+}
+
+func (u *UserService) Register(ctx context.Context, regReq *models.LoginRequest) error {
+	_, err := u.UserStorage.FindUserByLogin(ctx, regReq.Login)
+	if err == nil {
+		return fmt.Errorf("такой пользователь уже существует")
+	}
+
+	err = u.UserStorage.CreateUser(ctx, &models.User{Login: regReq.Login, Password: regReq.Password})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *UserService) CoinsHistory(ctx context.Context, login string) ([]models.CoinsEntry, error) {
+	user, err := u.UserStorage.FindUserByLogin(ctx, login)
+	if err != nil {
+		return nil, err
+	}
+
+	coinsHistory, err := u.UserStorage.CoinsHistory(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+	return coinsHistory, nil
+}
+
+func (u *UserService) PurchaseHistory(ctx context.Context, login string) ([]models.PurchaseEntry, error) {
+	user, err := u.UserStorage.FindUserByLogin(ctx, login)
+	if err != nil {
+		return nil, err
+	}
+
+	purchaseHistory, err := u.UserStorage.PurchaseHistory(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+	return purchaseHistory, nil
 }
