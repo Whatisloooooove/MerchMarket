5cec373a0b430865625a1303bfe6a41438546ceb
nakorneevay <nakorneevay@gmail.com>
2025-04-21 15:03:42 +0300

    Добавлена 2 итерация исправленного интерфейса Merch

--- new_version/internal/storage/entites/merch.go
+++ new_version/internal/storage/entites/merch.go
@@ -1,47 +1,24 @@
 package entites
 
-import "context"
-
-// Merch описывает id, название, стоимость и количество мерча.
-// Реализует сущность мерч в бд
-type Merch struct {
-	// ID содержит индификатор мерча
-	ID int
-
-	// Name содержит наименование мерча
-	Name string
-
-	// Price содержит стоимость одной позиции мерча
-	Price int
-
-	// Stock содержит количесвто единиц позиции мерча на складе
-	Stock int
-}
+import (
+	"context"
+	"merch_service/new_version/internal/models"
+)
 
 // MerchStorage определяет контракт для работы с товарами
 type MerchStorage interface {
-	// Create создает новый экземпляр Merch, возвращает ошибку
-	Create(ctx context.Context, merch *Merch) error
-	
-	// GetByID возвращает экземпляр Merch и ошибку по ID
-	GetByID(ctx context.Context, id int) (*Merch, error)
+	// Create создает новый экземпляр мерча, возвращает ошибку
+	Create(ctx context.Context, merch *models.Item) error
 
-	// GetByName возвращает экземпляр Merch и ошибку по Name
-	GetByName(ctx context.Context, id int) (*Merch, error)
-	
-	// GetList возвращает слайс Merch и ошибку
-	GetList(ctx context.Context) ([]*Merch, error)
-	
-	// Update обновляет информацию о Merch, возвращает ошибку
-	Update(ctx context.Context, merch *Merch) error
-	
-	// ReduceStock уменьшает количество товара на складе на qty, возвращает ошибку
-	ReduceStock(ctx context.Context, id int, qty int) error
-	
-	// IncreaseStock увеличивает количество товара на складе на qty, возвращает ошибку
-	IncreaseStock(ctx context.Context, id int, qty int) error
+	// GetByName возвращает по имени экземпляр мерча и ошибку
+	GetByName(ctx context.Context, name string) (*models.Item, error)
 
-	// Delete удаляет товар, возвращает ошибку
-	DeleteByID(ctx context.Context, id int) error
+	// GetList возвращает слайс всех мерчей и ошибку
+	GetList(ctx context.Context) ([]*models.Item, error)
+
+	// Update обновляет информацию о мерче, возвращает ошибку
+	Update(ctx context.Context, merch *models.Item) error
+
+	// Delete удаляет мерч по имени, возвращает ошибку
+	Delete(ctx context.Context, name string) error
 }
-
