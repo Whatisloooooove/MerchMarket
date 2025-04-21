eadcf65484c36798f6e8324d53e61d74e5e62989
nakorneevay <nakorneevay@gmail.com>
2025-04-21 15:27:20 +0300

    Оставлена самай простая реализация интерфейса по name

--- new_version/internal/storage/entites/merch.go
+++ new_version/internal/storage/entites/merch.go
@@ -7,18 +7,19 @@ import (
 
 // MerchStorage определяет контракт для работы с товарами
 type MerchStorage interface {
-	// Create создает новый экземпляр мерча, возвращает ошибку
+	// Create создает новый экземпляр мерча на основе экземпляра Item, возвращает ошибку
 	Create(ctx context.Context, merch *models.Item) error
 
-	// GetByName возвращает по имени экземпляр мерча и ошибку
-	GetByName(ctx context.Context, name string) (*models.Item, error)
+	// GetByName возвращает по name экземпляр мерча и ошибку
+	Get(ctx context.Context, name string) (*models.Item, error)
 
 	// GetList возвращает слайс всех мерчей и ошибку
 	GetList(ctx context.Context) ([]*models.Item, error)
 
-	// Update обновляет информацию о мерче, возвращает ошибку
-	Update(ctx context.Context, merch *models.Item) error
+	// Update обновляет информацию по name на основе экземпляра Item, возвращает ошибку
+	Update(ctx context.Context, name string, merch *models.Item) error
 
 	// Delete удаляет мерч по имени, возвращает ошибку
 	Delete(ctx context.Context, name string) error
 }
+
